3d83eb1e5fd385d0e1f9cab53a8d60e164b16412
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2022-04-27 21:35:16 +0200

    forgot one fetch

--- functions/GetImageUrl.js
+++ functions/GetImageUrl.js
@@ -1,6 +1,13 @@
 import useSWR from 'swr';
+const options = {
 
-const fetcher = url => fetch(url).then(res => res.blob()).then(res => URL.createObjectURL(res));
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    },
+}
+
+const fetcher = url => fetch(url, options).then(res => res.blob()).then(res => URL.createObjectURL(res));
 
 export default function GetImageUrl( imageName ) {
     const { data, error } = useSWR(`http://localhost:30005/getImage/${imageName}`, fetcher);
