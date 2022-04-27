8b9a65cc9b42993b0e9971c3ad1397278b9f3095
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2022-04-27 20:52:20 +0200

    fixed crossOrigin

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
                     <link 
                     href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;300;400&family=Roboto+Slab:wght@200;500&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,300&family=Rubik+Mono+One&family=Rubik:wght@500&display=swap" 
                     rel="stylesheet"/>
