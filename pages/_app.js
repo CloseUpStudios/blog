6abad8927cabb05ed26f8d3a58471baa0a31c70e
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2022-05-06 19:16:42 +0200

    added progressBar, fixed articleView text row

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,15 @@ import '../styles/buttonStyle.css'
 import '../styles/sidenavStyle.css'
 import '../styles/fonts.css'
 
+import NextNProgress from 'nextjs-progressbar'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress color="#F8CD86" />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
