e0f0be7fb4ecbc6c38f01255212cb41d7aedba59
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2022-05-23 21:36:04 +0200

    added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,18 +1,25 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
 const { createSecureHeaders } = require("next-secure-headers");
-
-const isDev = process.env.NODE_ENV !== "production";
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-  },
-  reactStrictMode: true,
-  images: {
-    domains: ["cdn.sanity.io"],
-  },
-  async headers() {
-    return [{ source: "/:path", headers: createSecureHeaders() }];
-  },
+const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: false,
 })
+
+
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  [withPWA],
+  {
+    pwa: {
+      dest: "public",
+    },
+    reactStrictMode: true,
+    images: {
+      domains: ["cdn.sanity.io"],
+    },
+    async headers() {
+      return [{ source: "/:path", headers: createSecureHeaders() }];
+    },
+  }
+])
