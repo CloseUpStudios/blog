37fe68a985ac30cb5edec68e339d02de0fbd8479
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2022-05-26 17:55:47 +0200

    added custom fonts back in

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,9 @@ export default function Header({title}) {
     return(
         <Head>
             <title>{title}</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+            <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto+Flex:opsz@8..144&family=Rubik+Mono+One&family=Vollkorn&display=swap" rel="stylesheet"></link>
         </Head>
     )
 }
